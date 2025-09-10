ac9e684832d3f9cdd0fb673df1462ee6a6ec4b07
Anas El-Gezawy <157417737+NsElgezawy@users.noreply.github.com>
2025-09-10 06:04:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const faders = document.querySelectorAll('.fade-in');
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
+        }
+    });
+});
+faders.forEach(fade => observer.observe(fade));
